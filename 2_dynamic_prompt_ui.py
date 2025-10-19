4efbcf4e3f69755a06b7a1bc144cfe494b2fa994
sjagadee <srini.jagadeesh@gmail.com>
2025-10-19 15:45:03 -0500

    create prompt template and making it reusable

--- 2_dynamic_prompt_ui.py
+++ 2_dynamic_prompt_ui.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import load_prompt
 from dotenv import load_dotenv
 import streamlit as st
 
@@ -23,19 +23,7 @@ style_input = st.selectbox("Select an Explanation Style",
 length_input = st.selectbox("Select a Length", 
                           ["Short (1 - 2 paragraphs)", "Medium (3 - 5 paragraphs)", "Long (detailes explained)"])
 
-prompt_template = PromptTemplate(
-    template="""Summarize the research paper titled "{paper_input}" with the following specifications:
-        Explanation Style: {style_input}
-        Explanation Length: {length_input}
-        1. Mathematical Details:
-            - Include relevant mathematical equations if present in the paper.
-            - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-        2. Analogies:
-            - Use relatable analogies to simplify complex ideas.
-        If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.
-        Ensure the summary is clear, accurate, and aligned with the provided style and length.""",
-    input_variables=["paper_input", "style_input", "length_input"]
-)
+prompt_template = load_prompt("template.json")
 
 prompt = prompt_template.invoke({
     "paper_input": paper_input,
